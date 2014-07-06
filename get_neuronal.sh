c18a4f2d9f9d0243bbd68eb74e6e6db5ba69ef1b
Valentin Haenel <valentin.haenel@gmx.de>
2014-07-06 20:40:41 +0200

    adding scripts for neuronal

---
+++ get_neuronal.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget http://vps.zaytsev.net/~zaytsev/prod-iaf-dst-cont-1000-ai-na-120.00m-1395826618.6076007.npz
+wget http://vps.zaytsev.net/~zaytsev/prod-iaf-dst-cont-1000-ai-na-120.00m-1395826618.6076007.npy
+
---
+++ mk_neuronal.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import numpy as np
+import bloscpack as bp
+
+print 'loading data..'
+z = np.load('prod-iaf-dst-cont-1000-ai-na-120.00m-1395826618.6076007.npz')
+d = dict((z.items()))
+d.pop('data_keys')
+a = np.hstack((d.values()))
+
+base = 2**(20)
+for name, size in (('small', base / 8),
+                    ('mid', base * 10 / 8),
+                    ('large', base * 100 / 8),
+                    ):
+    print 'save: %s' % name
+    np.save('neuronal_%sn' % name, a[:size])
+    bp.pack_ndarray_file(a[:size], 'neuronal_%sn.blp' % name)
