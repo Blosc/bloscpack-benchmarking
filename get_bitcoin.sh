5c83fca28640f11630ee857edbe4f01f773e56be
Valentin Haenel <valentin.haenel@gmx.de>
2014-07-21 00:25:57 +0200

    add my backup locations

--- get_bitcoin.sh
+++ get_bitcoin.sh
@@ -1,2 +1,4 @@
 !/bin/sh
 wget http://api.bitcoincharts.com/v1/csv/mtgoxUSD.csv.gz
+# in case the original fails
+# wget http://bloscpack-benchmarking.zetatech.org/mtgoxUSD.csv.gz
--- get_neuronal.sh
+++ get_neuronal.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 wget http://vps.zaytsev.net/~zaytsev/prod-iaf-dst-cont-1000-ai-na-120.00m-1395826618.6076007.npz
 wget http://vps.zaytsev.net/~zaytsev/prod-iaf-dst-cont-1000-ai-na-120.00m-1395826618.6076007.npy
-
+# in case the original fails
+# wget http://bloscpack-benchmarking.zetatech.org/prod-iaf-dst-cont-1000-ai-na-120.00m-1395826618.6076007.npz
+# wget http://bloscpack-benchmarking.zetatech.org/prod-iaf-dst-cont-1000-ai-na-120.00m-1395826618.6076007.npy
