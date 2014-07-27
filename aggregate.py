4a3b49d76a69e20c14a4469a8a7b95f43fb287f3
Valentin Haenel <valentin.haenel@gmx.de>
2014-07-27 08:36:00 +0200

    update the aggregate script

--- aggregate.py
+++ aggregate.py
@@ -3,27 +3,42 @@
 """ Award points for position. """
 
 import benchmark_analysis_utils as bau
+import pandas as pd
 import sys
 
+
+
+def aggregate(df, ratio=False):
+    values = ['compress', 'decompress', 'dc_no_cache']
+    if ratio:
+        values.append('ratio')
+    results = {}
+    for size in ('small', 'mid', 'large'):
+        for storage in ('ephemeral', 'esb'):
+            for complexity in ('arange', 'linspace', 'poisson', 'neuronal', 'bitcoin'):
+                for value in values:
+                    it = df.loc[(size, storage, complexity)].sort(value,
+                            ascending=value=='ratio')[value]
+                    if ratio:
+                        codecs = set(df.index.levels[-2]).difference(set(('tables', 'npy')))
+                        it = it.loc[codecs]
+                    for i,(index, value)in enumerate(it.iteritems(),start=1):
+                        #print i, "_".join(map(str,index)), value
+                        codec = "_".join(map(str,index))
+                        if codec not in results:
+                            results[codec] = i
+                        else:
+                            results[codec] += i
+    return results
+
 df = bau.load_results_file(sys.argv[1]).sort()
+df_results = pd.DataFrame.from_dict(aggregate(df, ratio=True), orient='index').sort(0)
+df_results.index.names = ('codec',)
+df_results.columns = ('score',)
+df_results.to_csv('aggregate_with_ratio.csv')
 
-results = {}
-
-for size in ('small', 'mid', 'large'):
-    for storage in ('ephemeral', 'esb'):
-        for complexity in ('arange', 'linspace', 'poisson', 'neuronal', 'bitcoin'):
-            for value in ('compress', 'decompress', 'dc_no_cache'):
-                it = df.loc[(size, storage, complexity)].sort(value, ascending=False)[value]
-                for i,(index, value)in enumerate(it.iteritems(),start=1):
-                    #print i, "_".join(map(str,index)), value
-                    codec = "_".join(map(str,index))
-                    if codec not in results:
-                        results[codec] = i
-                    else:
-                        results[codec] += i
-
-
-for i in sorted([(v,k) for k,v in results.items()]):
-    print i
-
-
+df = bau.load_results_file(sys.argv[1]).sort()
+df_results = pd.DataFrame.from_dict(aggregate(df, ratio=False), orient='index').sort(0)
+df_results.index.names = ('codec',)
+df_results.columns = ('score',)
+df_results.to_csv('aggregate_without_ratio.csv')
